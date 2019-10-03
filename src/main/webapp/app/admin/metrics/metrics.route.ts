71f843d5180034d0a976463c084e71c786a8e6cc
masoudbe <masoudbe@gmail.com>
2019-10-03 16:25:15 +0330

    Change mysql pass

--- src/main/webapp/app/admin/metrics/metrics.route.ts
+++ src/main/webapp/app/admin/metrics/metrics.route.ts
@@ -1,13 +1,8 @@
 import { Route } from '@angular/router';
 
 import { JhiMetricsMonitoringComponent } from './metrics.component';
-import {MsgtrackerComponent} from "app/msgtracker/msgtracker.component";
 
 export const metricsRoute: Route = {
-  path: 'jhi-metrics',
+  path: 'jhi-metrics'
   // component: JhiMetricsMonitoringComponent,
-  component: MsgtrackerComponent,
-  data: {
-    pageTitle: 'metrics.title'
-  }
 };
